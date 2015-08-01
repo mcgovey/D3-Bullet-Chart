29359d3b038caf4e1853098642a360ddd6ffbdb9
Kevin McGovern <kmcg82004@gmail.com>
2015-08-01 07:39:57 -0400

    Split function and encapsulated array creation

--- D3 Bullet Chart/D3-Bullet-Chart.js
+++ D3 Bullet Chart/D3-Bullet-Chart.js
@@ -1,5 +1,6 @@
 requirejs.config({
     shim: {
+      //Load d3 library before bullet.js
       //Only seems to work when extension name is hard coded
         "extensions/D3-Bullet-Chart/js/bullet": ["extensions/D3-Bullet-Chart/js/d3.min"]
     }
@@ -8,12 +9,40 @@ requirejs.config({
 define( [
         './js/properties'
         ,'jquery'
-        // ,'./js/d3.min'
         ,'./js/bullet'
         ,'css!./stylesheet.css'
     ],
-    function (props, $, style, bullet) {//d3, 
+    function (props, $, style, bullet) {
         'use strict';
+
+        var createDataArray = function (hypercubeData){
+          var dataObject = [],
+              numMeasures = hypercubeData.qMeasureInfo.length,
+              numDims     = hypercubeData.qDimensionInfo.length,
+              dataPages  = hypercubeData.qDataPages[0].qMatrix;
+
+                //Array creation
+                //Check the number of measures
+                if (numMeasures=2) {
+
+                  //Check the number of dimensions
+                  if (numDims!=0) {
+
+                for (var r = 0; r < dataPages.length; r++) {
+
+                  dataObject.push({ "title"   : dataPages[r][0].qText});
+                  dataObject[r]["markers"] = [Number(dataPages[r][1].qText.replace(",",""))];
+                  dataObject[r]["measures"] = [Number(dataPages[r][2].qText.replace(",",""))];
+                  dataObject[r]["ranges"] = [Number(dataPages[r][3].qText.replace(",",""))*.5,
+                                          Number(dataPages[r][3].qText.replace(",",""))*.75,
+                                          Number(dataPages[r][3].qText.replace(",",""))];
+
+                }
+                  }
+                }
+
+          return dataObject;
+        };
 
         return {
             definition: props,
@@ -33,37 +62,21 @@ define( [
 
 
                 var hc = layout.qHyperCube;
-          //------code to be used in creating cleaned hypercube array later
-                console.log("hc:", hc);
+                // console.log("hc:", hc);
 
-                var hcData = [];
+                // var hcData = [];
 
-                var numMeasures = hc.qMeasureInfo.length,
-                    numDims     = hc.qDimensionInfo.length,
-                    dataPages  = hc.qDataPages[0].qMatrix;
+                // var numMeasures = hc.qMeasureInfo.length,
+                //     numDims     = hc.qDimensionInfo.length,
+                //     dataPages  = hc.qDataPages[0].qMatrix;
 
-                //Array creation
-                //Check the number of measures
-                if (numMeasures=2) {
+                // var hcData = createDataArray();
 
-                  //Check the number of dimensions
-                  if (numDims!=0) {
 
-                    for (var r = 0; r < dataPages.length; r++) {
-                      
-                      hcData.push({ "title"   : dataPages[r][0].qText,
-                                    "markers" : [Number(dataPages[r][1].qText.replace(",",""))],
-                                    "measures": [Number(dataPages[r][2].qText.replace(",",""))],
-                                    "ranges"  : [Number(dataPages[r][3].qText.replace(",",""))*.5,
-                                                  Number(dataPages[r][3].qText.replace(",",""))*.75,
-                                                  Number(dataPages[r][3].qText.replace(",",""))]
-                                  });
+                var hcData = createDataArray(hc);
 
-                    }
-                  }
-                }
-
-                    console.log('hcData: ',hcData);
+                // console.log('hcDataType: ',typeof hcData);
+                console.log('hcData: ',hcData);
 
                 var margin = {top: 5, right: 20, bottom: 20, left: 40};
 
@@ -71,7 +84,7 @@ define( [
                 var width = $element.width() - margin.left - margin.right;
 
                 // Chart object height
-                var height = $element.height()/hcData.length - margin.top - margin.bottom - 10;//subtract 10 for bottom margin clipping
+                var height = ($element.height()/hcData.length) - margin.top - margin.bottom - 20;//subtract 20 for bottom margin clipping
 
                 // Chart object id
                 var id = "container_" + layout.qInfo.qId;
