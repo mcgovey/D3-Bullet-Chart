92fcef163452729eb2d908f00469664c98a0eb3e
Albert Backenhof <EXT_BPD@qlik.com>
2019-03-27 09:50:58 +0100

    Aligned build to Dashboard bundle extension builds
    -Part of the work to streamline how the extensions
     are handled, irregardless of what bundle.
    
    Issue: DEB-130, DEB-133

--- src/haiku.spec.js
+++
@@ -1,9 +0,0 @@
-/* https://poem-generator.org.uk */
-
-describe('Choking wintertime', () => {
-  describe('A solid, dirty ice cries', () => {
-    it('watching the sofa', () => {
-      expect(true).toBeTruthy();
-    });
-  });
-});
